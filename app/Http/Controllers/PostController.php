58568259ff154f3b049613598ab297458b02520f
Ahmed-Gamal-3afreT <ahmed.g201888@gmail.com>
2026-07-01 16:43:36 +0300

    PR Test

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class PostController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        return response()->json(['message' => 'ok']);
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        return response()->json(['message' => 'ok']);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        return response()->json(['message' => 'ok']);
+    }
+
+    public function update(Request $request, string $id): JsonResponse
+    {
+        return response()->json(['message' => 'ok']);
+    }
+
+    public function destroy(string $id): JsonResponse
+    {
+        return response()->json(['message' => 'ok']);
+    }
+}
