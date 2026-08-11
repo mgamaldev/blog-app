5b4ddf4abc4e6364d03a7420c1e262892d23efcb
Ahmed-Gamal-3afreT <ahmed.g201888@gmail.com>
2026-08-12 00:56:47 +0300

    Impelment comments and tags apis with policy protection

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\TagController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::apiResource('posts', PostController::class)->only(['index', 'show']);
+Route::get('posts/{post}/comments', [CommentController::class, 'index']);
+
 Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
+Route::get('tags', [TagController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
@@ -20,5 +25,9 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::apiResource('posts', PostController::class)->except(['index', 'show']);
+    Route::post('posts/{post}/comments', [CommentController::class, 'store']);
+    Route::delete('comments/{comment}', [CommentController::class, 'destroy']);
+    Route::post('tags', [TagController::class, 'store']);
+    Route::put('posts/{post}/tags', [TagController::class, 'syncTags']);
     Route::apiResource('categories', CategoryController::class)->except(['index', 'show']);
 });
